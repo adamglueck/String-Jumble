2e21c3b653cf0e14aa063a4af61e7c4f31358b00
adamglueck <adam.glueck1@gmail.com>
2016-02-22 14:21:50 -0500

    Updated from Brython Server: 2/22/2016 2:21:50 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,3 +26,9 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 text=input("Please enter a string of text (the bigger the better): ")
+count=len(text)
+words=list(text)
+backwords=[]
+for i in range (1,(count+1)):
+    backwords.append(-i)
+print(backwords)

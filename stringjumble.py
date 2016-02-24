28d788124370285166e25929df8fd8df7e8d5cf3
adamglueck <adam.glueck1@gmail.com>
2016-02-24 13:30:51 -0500

    Updated from Brython Server: February 24, 2016 1:30:53 PM EST

--- stringjumble.py
+++ stringjumble.py
@@ -2,23 +2,17 @@
 stringjumble.py
 Author: <your name>
 Credit: 
-
 Assignment:
-
 The purpose of this challenge is to gain proficiency with 
 manipulating lists.
-
 Write and submit a Python program that accepts a string from 
 the user and prints it back in three different ways:
-
 * With all letters in reverse.
 * With words in reverse order, but letters within each word in 
   the correct order.
 * With all words in correct order, but letters reversed within 
   the words.
-
 Output of your program should look like this:
-
 Please enter a string of text (the bigger the better): There are a few techniques or tricks that you may find handy
 You entered "There are a few techniques or tricks that you may find handy". Now jumble it:
 ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
@@ -42,13 +36,10 @@ for i in range (1,count):
     if backwords [i]==" ":
         spaces.append(i+1)
         spacecounter=spacecounter+1
-list=[]
 backwurds=""
 print(spaces)
 for i in range (1,spacecounter,-1):
-    for l in range (spaces[i],spaces[i]):
-        backwurds=backwords[l]+backwurds
-        list.append(backwurds)
-
+    for l in range (spaces[i]+1,spaces[i+1]-1):
+        backwurds=backwurds+backwords[l]
 print(backwurds)
 """store a string as a list of words"""
